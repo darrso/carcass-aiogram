5a82f41ac84f2e9ea852bd723fbe66aba96373ea
darrso <lfif1123@yandex.ru>
2022-01-05 21:25:43 +0300

    set_commands

--- FSMBot/main.py
+++ FSMBot/main.py
@@ -10,12 +10,15 @@ from python.config import bToken
 from python.handlers.message_handlers import register_message_handlers
 from python.handlers.query_handlers import register_query_handlers
 
+
 async def set_commands(bot: Bot):
     await bot.set_my_commands(
         [BotCommand(command="/start", description='Start Command'),  # start
          BotCommand(command="/que", description='Give Answers :)'),  # que
          ]
     )
+
+
 async def main():
     bot = Bot(token=bToken)
     dp = Dispatcher(bot, storage=MemoryStorage())
